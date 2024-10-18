043c295115e46a53197f3a7f1206350a1975ba7a
Sam1713 <samsaju399@gmail.com>
2024-10-18 12:02:42 +0530

    mongodb connected

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,18 @@
 import express from 'express';
-
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+dotenv.config();
+mongoose.connect(process.env.MONGOURL as string)
+.then(()=>{
+  console.log("Connected to MongoDb");
+})
+.catch((error)=>{
+  console.log(error);
+  
+})
 const app = express();
 const PORT = 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
