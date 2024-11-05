f66385f8c0e8fc2ff5360f6ec0cf0ca16d006e6a
Sam1713 <samsaju399@gmail.com>
2024-11-05 13:32:53 +0530

    some comments removed again

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,17 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser'; // Import cookie-parser
-import dotenv from 'dotenv'; // Import dotenv
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv'; 
 
 import connectDB from './config/config';
-import userRoute from './routes/userRoute'; // Adjust path if necessary
-import authRoute from './routes/authRoute'; // Adjust path if necessary
+import userRoute from './routes/userRoute'; 
+import authRoute from './routes/authRoute'; 
 
-// Load environment variables from .env file
 dotenv.config();
 
 const corsOptions = {
   origin: ['http://localhost:5173', 'https://image-tamer-6jqy.vercel.app'], // Allow both local and production frontend
-  credentials: true, // Allow credentials (cookies, tokens, etc.)
+  credentials: true, 
 };
 
 
@@ -25,16 +24,13 @@ const PORT = process.env.PORT || 3000; // Use PORT from environment variables or
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Define routes
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 
-// Error-handling middleware
 app.use((err: any, _req: Request, res: Response, _next: NextFunction): void => {
-  const statusCode = err.statusCode || 500; // Accessing statusCode on any error
+  const statusCode = err.statusCode || 500; 
   const message = err.message || "Internal server error";
 
   res.status(statusCode).json({
